4d69937f2f35a772329e8ffb88bb0e4e3f8b8ffd
Marlon Montesa <montesamarlon20@gmail.com>
2023-12-27 15:05:25 +0800

    first commit

---
+++ crud-sql/app/config/db.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  HOST: "LAPTOP-C6LR1RAI",
+  PORT: "1434",
+  USER: "marlon",
+  PASSWORD: "12345",
+  DB: "marlon_db",
+  dialect: "mssql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+
